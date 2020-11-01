e94358a508ca84e07166c433e6ff1e0372a1ef89
elahe dastan <elahe.dstn@gmail.com>
2020-11-01 23:45:20 +0330

    source command

---
+++ commands.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# redirect
+ls > files
+
+# source command reads and executes the content of a file
+source gfg.txt
--- env-check.sh
+++ env-check.sh
@@ -13,6 +13,3 @@ if [ ! -f ".env" ]; then
   echo ".env doesn't exist"
   exit 1
 fi
-
-# redirect
-ls > files
